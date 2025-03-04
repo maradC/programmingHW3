6ed262d28c259ccd3f2c5e8bad1adbd6bb4d8869
Chris M <maradiagaChris03@gmail.com>
2025-03-03 22:26:02 -0500

    creating address

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        int PAGE_MAX = 100;
+        int[] processes = new int[PAGE_MAX];
+        for (int i = 0; i < processes.length; i++) {
+            processes[i] = -1;
+        }
+        userMemoryAllocation(processes);
     }
     public static void userMemoryAllocation(int[] processes) {
+        Random random = new Random();
+        int currentAddress = 2000;
+        int currentProcess = 0;
 
 
     }
